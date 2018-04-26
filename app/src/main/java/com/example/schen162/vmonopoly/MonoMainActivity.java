a936440a007b2b26435a226d35d79cdaf32ecf83
Chen <SCHEN162@ford.com>
2018-04-26 14:47:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/schen162/vmonopoly/MonoMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.schen162.vmonopoly;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MonoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mono_main);
+    }
+}
