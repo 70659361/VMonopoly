24e6a102db9db91b66134a3c608d1136c6b8b569
SCHEN162 <70659361@qq.cm>
2018-04-26 17:05:05 +0800

    configuration

--- app/src/main/java/com/example/schen162/vmonopoly/MonoMainActivity.java
+++ app/src/main/java/com/example/schen162/vmonopoly/MonoMainActivity.java
@@ -2,6 +2,8 @@ package com.example.schen162.vmonopoly;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
@@ -9,20 +11,49 @@ import com.amap.api.maps.MapView;
 
 public class MonoMainActivity extends AppCompatActivity {
 
-    MapView mMapView = null;
-    AMap aMap = null;
+    private com.amap.api.maps2d.MapView mapView;
+    private com.amap.api.maps2d.AMap aMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mono_main);
 
-        mMapView = (MapView) findViewById(R.id.map);
-        mMapView.onCreate(savedInstanceState);
+        mapView = (com.amap.api.maps2d.MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
 
+        init();
+    }
 
+    private void init() {
         if (aMap == null) {
-            aMap = mMapView.getMap();
+            aMap = mapView.getMap();
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mapView.onSaveInstanceState(outState);
     }
 }
